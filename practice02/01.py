444538c4dda2eeb05b19967f205de0eaca218e00
hj5730 <hj5730@naver.com>
2021-03-31 17:38:41 +0900

    파이썬 연습문제 1

---
+++ practice02/01.py
@@ -0,0 +1,7 @@
+# 문제1
+# 파이썬 경로명 s = '/user/local/bin/python' 에서 각각의 디렉토리 경로명을 분리하여 출력하세요.
+# 슬라이싱으로 해서 쪼갠다음에 스프릿
+s = '/user/local/bin/python'
+
+
+# 또,
---
+++ practice02/05.py
@@ -0,0 +1,11 @@
+# 문제5.
+# 함수 sum 을 만드세요. 이 함수는 임의의 개수의 인수를 받아서 그 합을 계산합니다.
+
+# 가변함수.
+
+def mysum():
+    pass
+
+print(mysum())
+print(mysum(1, 2))
+print(mysum(1, 2, 5, 7, 2, 3))
