f2bc3d3f1b433e075c0867071c841a1ee0746cce
hj5730 <hj5730@naver.com>
2021-04-06 11:49:01 +0900

    python class test 2

--- class_test/pointtest.py
+++ class_test/pointtest.py
@@ -6,5 +6,11 @@ p1.draw() # p1의 포인트를 draw함
 
 p2 = ColorPoint('red', 30, 50)
 p2.draw()
+print(type(p2)) # 함수 __str__을 호출해서 프린트해줌
+print(p2)
+
+i = 10
+print(type(i))
+print(i)
 
 print(Point.count_of_instance) # 객체가 2개 생겼으니까 2라고 출력됨. (p1.draw, p2.draw)
